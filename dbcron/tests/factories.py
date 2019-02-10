b96e08ce811801268fe455017318727f0986c6ab
Anthony Monthe <anthony.monthe@gmail.com>
2019-02-10 02:14:32 +0000

    Added JobFactory

--- dbcron/tests/factories.py
+++ dbcron/tests/factories.py
@@ -1,3 +1,22 @@
 from faker import Faker
+import factory
+from factory import fuzzy
 
 faker = Faker()
+
+
+class JobFactory(factory.django.DjangoModelFactory):
+    name = factory.Faker('job')
+    tag = factory.Faker('name')
+    func = 'django.utils.timezone.now'
+
+    sec = 0
+    min = fuzzy.FuzzyInteger(0, 59)
+    hou = fuzzy.FuzzyInteger(0, 23)
+    dom = '*'
+    mon = '*'
+    dow = '*'
+    yea = '*'
+
+    class Meta:
+        model = 'dbcron.Job'
