5d4ffd35939c3dda052186e9a2a9854b99b78196
Anthony Monthe <anthony.monthe@gmail.com>
2019-02-11 13:07:58 +0000

    Added AbstractTable

--- dbcron/contrib/tables.py
+++ dbcron/contrib/tables.py
@@ -7,14 +7,17 @@ from dbcron import models
 from dbcron import utils
 
 
-class JobTable(tables.Table):
+class AbstractJobTable(tables.Table):
     next_time = columns.Column(
         orderable=False, verbose_name=_("Next run"))
 
+
+    def render_next_time(self, value, record):
+        return utils.get_next_time(record)
+
+
+class JobTable(AbstractJobTable, tables.Table):
     class Meta:
         model = models.Job
         fields = sequence = ['name', 'tag', 'func', 'is_active', 'next_time',
                              'sec', 'min', 'hou', 'dom', 'mon', 'dow', 'yea']
-
-    def render_next_time(self, value, record):
-        return utils.get_next_time(record)
--- dbcron/views.py
+++ dbcron/views.py
@@ -20,14 +20,22 @@ class JobCalendarMixin:
         calendar_kwargs = self.get_calendar_kwargs()
         return klass(**calendar_kwargs)
 
-
-class JobMonthCalendarMixin(JobCalendarMixin):
     def get_context_data(self, *args, **kwargs):
         data = super().get_context_data(*args, **kwargs)
         calendar = self.get_calendar()
         month_calendar = calendar.formatmonth(now().year, now().month)
         data.update({
             'calendar': calendar,
+        })
+        return data
+
+
+class JobMonthCalendarMixin(JobCalendarMixin):
+    def get_context_data(self, *args, **kwargs):
+        data = super().get_context_data(*args, **kwargs)
+        calendar = data['calendar']
+        month_calendar = calendar.formatmonth(now().year, now().month)
+        data.update({
             'month_calendar': mark_safe(month_calendar),
         })
         return data
@@ -36,10 +44,14 @@ class JobMonthCalendarMixin(JobCalendarMixin):
 class JobWeekCalendarMixin(JobCalendarMixin):
     def get_context_data(self, *args, **kwargs):
         data = super().get_context_data(*args, **kwargs)
-        calendar = self.get_calendar()
-        week_calendar = calendar.formatweekofmonth(now().year, now().date().isocalendar()[1])
+        now_ = now()
+        current_week = now_.date().isocalendar()[1]
+        week = kwargs.get('week', current_week)
+        year = kwargs.get('year', now_.year)
+        calendar = data['calendar']
+        week_calendar = calendar.formatweekofmonth(year, week)
         data.update({
-            'calendar': calendar,
             'week_calendar': mark_safe(week_calendar),
+            'current_week': current_week,
         })
         return data
