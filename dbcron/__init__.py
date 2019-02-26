f6ca72be83aa2adf4047083089966ffba07fc555
Anthony Monthe <anthony.monthe@gmail.com>
2019-02-26 00:05:59 +0000

    Bump

--- dbcron/__init__.py
+++ dbcron/__init__.py
@@ -1,5 +1,5 @@
 """Manage your crontab directly from DB."""
-VERSION = (0, 5, 1)
+VERSION = (0, 5, 2)
 __version__ = '.'.join([str(i) for i in VERSION])
 __author__ = 'Anthony Monthe (ZuluPro)'
 __email__ = 'amonthe@cloudspectator.com'
